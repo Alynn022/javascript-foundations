1333e887978133fece535f2b0ed1316b8dccb1c0
Lynn Nguyen <AmandaN22@outlook.com>
2021-10-18 17:11:17 -0600

    Pass all unicorn.js tests

--- mythical-creatures/exercises/unicorn.js
+++ mythical-creatures/exercises/unicorn.js
@@ -1,8 +1,24 @@
 class Unicorn {
-  constructor(name, color) {
+  constructor(name, color = 'white') {
     this.name = name;
     this.color = color;
+  }
+
+  isWhite() {
+    return this.color === 'white' 
+  }
+
+  says(greeting) {
+    return `**;* ${greeting} *;**`
   }
 }
 
 module.exports = Unicorn;
+
+
+
+
+// 1. read the test that you're running
+// 2. ask yourself, if it's going to fail and why?
+// 3. run the test
+// 4. make it pass
