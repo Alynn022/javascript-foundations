be8d0988681ca3cede6ed227156307c6764946a6
Lynn Nguyen <AmandaN22@outlook.com>
2021-10-18 20:47:11 -0600

    pass dragon.js test

--- mythical-creatures/exercises/dragon.js
+++ mythical-creatures/exercises/dragon.js
@@ -1,1 +1,20 @@
+class Dragon {
+  constructor(name, rider) {
+    this.name = name;
+    this.rider = rider;
+    this.hungry = true
+    this.eatCount = 0
+  }
+  greet(greeting) {
+    return `Hi, ${this.rider}!`;
+  }
+  eat() { //there are no parameters because the test is not passing an argument
+    this.eatCount ++ //++ is adding 1 to the count 
+    if(this.eatCount > 2) {
+      this.hungry = false;
+    }
+  }
+};
 
+
+module.exports = Dragon;
