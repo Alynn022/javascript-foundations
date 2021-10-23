803a9740ee67327e0c2ae5e628165e6f85940d6e
Lynn Nguyen <AmandaN22@outlook.com>
2021-10-22 20:31:04 -0600

    Update human test

--- do-you-wanna-build-a-snowman/src/Human.js
+++ do-you-wanna-build-a-snowman/src/Human.js
@@ -1,7 +1,23 @@
 var Snowman = require('./Snowman');
 
 class Human {
-
-}
+  constructor(name) {
+    this.name = name;
+    this.wantsToBuildASnowman = true;
+    this.materials = {
+      snowballs: 0,
+      coal: 0,
+      buttons: 0,
+      carrots: 0,
+    }
+  }
+    gatherMaterials(material, itemNum) {
+      this.materials[material] += itemNum;
+    //bracket notation to pass in keys that are changing.
+  }
+    buildASnowman(Snowman) {
+      console.log(this.materials.coal)
+    }
+  }
 
 module.exports = Human;
