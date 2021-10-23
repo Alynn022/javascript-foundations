09bd55e94854fe6d8dc8c4357f1df9c80d119cb4
Lynn Nguyen <AmandaN22@outlook.com>
2021-10-22 20:30:09 -0600

    Update snowman

--- do-you-wanna-build-a-snowman/src/Snowman.js
+++ do-you-wanna-build-a-snowman/src/Snowman.js
@@ -1,5 +1,25 @@
 class Snowman {
-
+  constructor(snowmanDetail) {
+    this.carrots = snowmanDetail.carrots;
+    this.coal = snowmanDetail.coal;
+    this.buttons = snowmanDetail.buttons;
+    this.snowballs = snowmanDetail.snowballs;
+    this.magicHat = false;
+  }
+  canWearMagicHat() {
+    if(this.coal < 2) {
+      this.magicHat = false
+    } else if(this.buttons < 5) {
+      this.magicHat = false;
+    } else if(this.carrots < 1) {
+      this.magicHat = false;
+    } else if(this.snowballs < 2) {
+      this.magicHat = false;
+    } else {
+      this.magicHat = true
+    }
+  }
 }
+
 
 module.exports = Snowman;
