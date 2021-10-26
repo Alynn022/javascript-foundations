c052942fc7106becfe931a6db66be0f2d9cdd200
Lynn Nguyen <AmandaN22@outlook.com>
2021-10-26 16:01:01 -0600

    Pass hobbit tests

--- mythical-creatures/exercises/hobbit.js
+++ mythical-creatures/exercises/hobbit.js
@@ -1,21 +1,29 @@
 class Hobbit {
   constructor(hobbit) {
     this.name = hobbit.name;
-    this.age = hobbit.age = 0;
+    this.age = 0;
+    this.adult = false
+    this.old = false
+    this.hasRing = false
   }
   celebrateBirthday() {
     (this.age ++)
+    if (this.age >= 33) {
+      this.adult = true
+    }
+    if (this.age >= 101) {
+     this.old = true
+    }
   }
-  timeTravel() {
-    console.log(hobbit)
+  getRing(phrase) {
+    if (this.name === 'Frodo') {
+      this.hasRing = true
+      return `Here is the ring!`
+    } else {
+      return `You can\'t have it!`
+    }
   }
 }
-
-
-// if(this.age < 33)
-// this.adult = hobbit.adult = false
-// else hobbit.adult = true
-
 
 
 module.exports = Hobbit;
