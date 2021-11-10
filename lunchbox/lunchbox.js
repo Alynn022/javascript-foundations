78dce29da147a018cd83e19a76f30c11669488ca
Lynn Nguyen <AmandaN22@outlook.com>
2021-11-09 23:04:10 -0700

    Pass all lunchbox and snack test suites

--- lunchbox/lunchbox.js
+++ lunchbox/lunchbox.js
@@ -1,5 +1,49 @@
 class LunchBox {
+  constructor(lunchBoxDetails) {
+    this.owner = lunchBoxDetails.owner; 
+    this.material = lunchBoxDetails.madeOf;
+    this.shape = lunchBoxDetails.shape; 
+    this.color = lunchBoxDetails.color;
+    this.snacks = [];
+  }
+  acquireSnack(snack) {
+    this.snacks.push(snack);
+    snack.isInLunchBox = true; 
+  }
 
+  findHealthySnacks() {
+    var healthySnacks = []
+    for (var i = 0; i < this.snacks.length; i++) {
+      if (this.snacks[i].checkForHealthy()) {
+        healthySnacks.push(this.snacks[i].type);
+      }
+    }
+    return healthySnacks;
+  }
 }
+
+//   findHealthySnacks() {
+//     var healthySnacks = []
+//     for (var i = 0; i < this.snacks.length; i++) {
+//       if (this.snacks[i].type.includes('Fruit') || this.snacks[i].type.includes('fruit')) {
+//         healthySnacks.push(this.snacks[i].type);
+//       } 
+//     } 
+//     return healthySnacks
+//   }
+
+//   findHealthySnacks() {
+//     // needs to return an array of two strings
+//     // ["Mixed fruit", "Fruit"]
+//     var healthySnacks = [];
+//     for (var i = 0; i< this.snacks.length; i++) {
+//       if (this.snacks[i].type.toUpperCase().includes('FRUIT')) {
+//         healthySnacks.push(this.snacks[i].type)
+//       }
+//     }
+//     return healthySnacks;
+//   }
+// }
+
 
 module.exports = LunchBox;
--- lunchbox/snack.js
+++ lunchbox/snack.js
@@ -1,5 +1,26 @@
 class Snack {
-
+  constructor(type) {
+    this.deliciousLevel = 'extra';
+    this.type = type;
+    this.amount = 100;
+    this.isInLunchBox = false;
+    this.cuttingItClose = false;
+  }
+  getEaten() {
+    // no return
+    // needs to change this.cuttingItClose`
+    this.amount -= 10;
+    if (this.amount <= 20) {
+      this.cuttingItClose = true; 
+    }
+  }
+  checkForHealthy() {
+    if (this.type.includes('Fruit') || this.type.includes('fruit')) {
+      return true;
+    } else {
+      return false; 
+    }
+  }
 }
 
 module.exports = Snack;
